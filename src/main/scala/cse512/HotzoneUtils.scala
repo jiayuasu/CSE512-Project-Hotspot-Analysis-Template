4087e438c0ea7991142bbf53228acf6bd6f7948a
Nipun Mediratta <nipunmediratta@gmail.com>
2019-11-16 00:33:36 -0700

    HotZone Utils done
    Implemented and tested

--- src/main/scala/cse512/HotzoneUtils.scala
+++ src/main/scala/cse512/HotzoneUtils.scala
@@ -2,9 +2,11 @@ package cse512
 
 object HotzoneUtils {
 
-  def ST_Contains(queryRectangle: String, pointString: String ): Boolean = {
-    // YOU NEED TO CHANGE THIS PART
-    return true // YOU NEED TO CHANGE THIS PART
+  def StContains(rectPoints: Array[Double],points: Array[Double]):Boolean = {
+    if (points(0)>=rectPoints(0) && points(0)<=rectPoints(2) && points(1)>=rectPoints(1) && points(1)<=rectPoints(3)){
+      return true;
+    }
+    return false;
   }
 
   // YOU NEED TO CHANGE THIS PART
